57dc15802651a45251a855c35b378dc5dae8c887
Jared Stromberg <jaredstromberg@gmail.com>
2020-07-15 17:10:22 -0600

    Modified the routes.rb file to mount the Action Cable server on a sub-URI of the main application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   resources :messages
-  resources :chatrooms
+  resources :chatrooms, param: :slug
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
