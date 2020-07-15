2a580b5d2805a2fbfb6143a0f58b56d7b42846ee
Jared Stromberg <jaredstromberg@gmail.com>
2020-07-15 17:15:10 -0600

    Annotated app/javascript/consumer.js to highlight it was pre-built and defined by rails new. Unknown at this time if this will interfere with the desired functionality of the tutorial.

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -4,3 +4,8 @@
 import { createConsumer } from "@rails/actioncable"
 
 export default createConsumer()
+
+
+// This is predefined by rails new, though the tutorial (using Rails 5) states this needs to be created in a slightly different directory.
+// TBD if this will still function the way it is intended by following the tutorial or if further tweaking needs to take place to 
+// duplicate the desired functionality.
